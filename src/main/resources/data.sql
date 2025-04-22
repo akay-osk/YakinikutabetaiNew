9960b13c4a206a8133a1cb4433750efba4c8ef61
3030929 <3030929@30309-29>
2025-04-22 12:54:39 +0900

    Merge branch 'master' of https://github.com/chawanmushi-tabetai/yakiniku-tabetai.git

---
+++ src/main/resources/data.sql
@@ -0,0 +1,19 @@
+-- user 1件目
+INSERT INTO users(user_pass,user_name,user_age,user_gender,user_likes,user_bugyo,user_smoker,
+user_drinker,user_detail,user_good,user_bad,user_address)
+VALUES
+('aaa','じろう',35,false,'上ミノ',false,false,true,'お酒が本体',0,0,'aba@aaa');
+
+-- user 2件目
+INSERT INTO users(user_pass,user_name,user_age,user_gender,user_likes,user_bugyo,user_smoker,
+user_drinker,user_detail,user_good,user_bad,user_address)
+VALUES
+('bbb','ハラミちゃん',25,true,'ハラミ',true,false,false,'ピアノ弾けません',0,0,'ccc@paa');
+
+-- user 3件目
+INSERT INTO users(user_pass,user_name,user_age,user_gender,user_likes,user_bugyo,user_smoker,
+user_drinker,user_detail,user_good,user_bad,user_address)
+VALUES
+('ccc','すたみな太郎',28,false,'カルビ',false,false,false,'米も喰え！！',0,0,'bbb@kar');
+
+
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,42 +1,53 @@
-/*drop table if exists user cascade;
+drop table if exists users cascade;
+drop table if exists blocking cascade;
 drop table if exists matching cascade;
 drop table if exists room cascade;
 drop table if exists chat cascade;
-drop table if exists blocking cascade;
-*/
+
 
 
 
 --ユーザーテーブル
-CREATE TABLE user(
+CREATE TABLE users(
 	-- ユーザーID : 主キー
 	user_id SERIAL PRIMARY KEY,
 	-- 外部認証 : 
 	user_pass VARCHAR(100) NOT NULL,
 	-- 登録名
-	user_name VARCHAR(20) NOT NULL,
+	user_name VARCHAR(50) NOT NULL,
 	-- 年齢
 	user_age INTEGER NOT NULL,
 	-- 性別
 	user_gender BOOLEAN NOT NULL,
 	-- 好きな部位
-	user_likes VARCHAR(20),
+	user_likes VARCHAR(50),
 	-- 焼肉奉行
-	user_bugyo BOOLEAN,
+	user_bugyo BOOLEAN DEFAULT false,
 	-- 喫煙者か
-	user_smoker BOOLEAN,
+	user_smoker BOOLEAN DEFAULT false,
 	-- 飲酒するか
-	user_drinker BOOLEAN,
+	user_drinker BOOLEAN DEFAULT false,
 	-- 紹介文
 	user_detail VARCHAR(50),
 	-- 評価good
-	user_good INTEGER,
+	user_good INTEGER DEFAULT 0,
 	-- 評価bad
-	user_bad INTEGER,
+	user_bad INTEGER DEFAULT 0,
 	-- アイコン
-	user_icom BYTEA,
+	user_icon BYTEA,
 	-- アドレス
-	user_address VARCHAR(100) NOT NULL,
+	user_address VARCHAR(255) NOT NULL
+);
+
+
+-- ブロックユーザーテーブル
+CREATE TABLE blocking(
+	-- ユーザーID
+	 user_id INTEGER REFERENCES users(user_id),
+	-- 上記ユーザーIDがブロックしているユーザーID
+	blocking_user_id INTEGER REFERENCES users(user_id),
+	  -- 一意制約を追加
+    CONSTRAINT unique_block UNIQUE (user_id, blocking_user_id)
 );
 
 
@@ -49,7 +60,7 @@ CREATE TABLE matching(
 	-- 時間
 	matching_time INTEGER NOT NULL,
 	-- 会員ID : user_id引用
-	user_id INTEGER NOT NULL REFERENCES user(user_id),
+	user_id INTEGER NOT NULL REFERENCES users(user_id),
 	-- 性別 
 	matching_gender BOOLEAN,
 	-- 年齢
@@ -57,7 +68,7 @@ CREATE TABLE matching(
 	-- 人数
 	matching_member INTEGER,
 	-- ブロックしているユーザー
-	blocking_user_id INTEGER REFERENCES user(blocking_user_id),
+	blocking_user_id INTEGER REFERENCES users(user_id)
 	);
 
 -- ルームテーブル
@@ -65,9 +76,9 @@ CREATE TABLE room(
  	-- ルームID : 主キー
 	room_id SERIAL PRIMARY KEY,
 	-- ユーザーID
-	user_id INTEGER REFERENCES user(user_id),
+	user_id INTEGER REFERENCES users(user_id),
 	--　ルーム期限
-	delete_at INTEGER,
+	delete_at TIMESTAMP,
 	-- マッチング希望条件id
 	matching_id INTEGER REFERENCES matching(matching_id)
 );
@@ -77,9 +88,9 @@ CREATE TABLE chat(
 	-- ルームID 
 	room_id INTEGER REFERENCES room(room_id),
 	-- ユーザーID
-	user_id INTEGER REFERENCES user(user_id),
+	user_id INTEGER REFERENCES users(user_id),
 	-- 登録名
-	user_name VARCHAR(20) REFERENCES user(user_name),
+	user_name VARCHAR(20),
 	-- コメント
 	chat_comment TEXT,
 	-- 時間
@@ -87,11 +98,4 @@ CREATE TABLE chat(
 );
 
 
--- ブロックユーザーテーブル
-CREATE TABLE blocking(
-	-- ユーザーID
-	 user_id INTEGER user(user_id),
-	-- 上記ユーザーIDがブロックしているユーザーID
-	blocking_user_id INTEGER user(user_id)
-);
 
