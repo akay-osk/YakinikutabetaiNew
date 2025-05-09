33fa7e0d83b9a964caca7ac61504917c9cd837dd
3030838 <3030838@30308-38>
2025-05-09 15:04:53 +0900

    a

--- src/main/java/com/example/demo/security/CustomUserDetails.java
+++ src/main/java/com/example/demo/security/CustomUserDetails.java
@@ -5,20 +5,29 @@ import java.util.Collection;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class CustomUserDetails implements UserDetails{
 	private final int userId;
 	private final int roomId;
-	private final String username;
-	private final String password;
 	
+
 	//今回は権限ないのでnull
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO 自動生成されたメソッド・スタブ
 		return null;
 	}
 }
