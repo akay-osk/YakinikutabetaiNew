69ab7efe0ad7f85e0801aaf5efa5b13f0ca435e3
3030928 <3030928@30309-28>
2025-05-09 13:38:38 +0900

    新規登録アイコン画像処理、タグ登録処理修正。プロフィール編集画面作成。UserProfileEdit.htmlをSakai.htmlにリネーム。

--- src/main/java/com/example/demo/service/impl/UsersServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UsersServiceImpl.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
 import com.example.demo.entity.User;
 import com.example.demo.mapper.UsersMapper;
@@ -57,12 +58,31 @@ public class UsersServiceImpl implements UsersService {
 	
 	@Override
 	public void updateUsers(User users) {
-		//アップデート時もハッシュ化 キタガワ
-		if(users.getUser_pass() != null && !users.getUser_pass().isEmpty()) {
-	    String hashedPass = passwordEncoder.encode(users.getUser_pass());
-	    users.setUser_pass(hashedPass);
-	   }
-		usersMapper.update(users);
+		/// 既存のユーザー情報を取得
+	    User existingUser = usersMapper.selectByIdUsers(users.getUser_id());
+
+	    // パスワードの更新判定（入力時ハッシュ化して更新）
+	    if (StringUtils.hasText(users.getNewPassword())) { 
+	        String hashedPass = passwordEncoder.encode(users.getNewPassword());
+	        users.setUser_pass(hashedPass);
+	    } else {// 未入力なら既存のパスワードを保持
+	    	users.setUser_pass(existingUser.getUser_pass());
+	    }
+	    
+	    // アイコンの更新判定（登録があれば画像を保持）
+	    if (users.getIconFile() != null && !users.getIconFile().isEmpty()) {
+	    	try {
+	    		users.setUser_icon(users.getIconFile().getBytes());
+	        } catch (IOException e) {
+	            throw new RuntimeException("画像の読み込みに失敗しました", e);
+	        }
+	    } else {// 未登録なら元の画像を保持
+	        users.setUser_icon(existingUser.getUser_icon()); 
+	    }
+
+	    // ユーザー情報を更新
+	    usersMapper.update(users);
+
 	}
 
 	@Override
