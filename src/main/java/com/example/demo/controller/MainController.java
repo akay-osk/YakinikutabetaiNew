077de37641ac3e4474641d38278e310ca2085cfa
3030838 <3030838@30308-38>
2025-05-14 15:27:47 +0900

    form -> dto converted

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.demo.dto.LoginForm;
 import com.example.demo.entity.User;
-import com.example.demo.form.LoginForm;
 
 /*
  * MainControllerクラス
--- src/main/java/com/example/demo/dto/LoginForm.java
+++ src/main/java/com/example/demo/dto/LoginForm.java
@@ -1,4 +1,4 @@
-package com.example.demo.form;
+package com.example.demo.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
